ec61dcab0d666289c088eda97429c1e58a2ccbb4
Edwar-Forero <edwar.forero@correounivalle.edu.co>
2026-05-02 10:28:56 -0500

    guardado de datos procesados

--- etl/__init__.py
+++ etl/__init__.py
@@ -1,10 +1,9 @@
-from .cleaning import parsear_columnas_eliminar, eliminar_columnas, eliminar_duplicados
+from .cleaning import  eliminar_columnas, eliminar_duplicados
 from .nulls import manejar_nulos
 from .normalization import estandarizar_nombres_columnas, ajustar_tipos_datos
 from .schema import VALID_COLUMNS_NACIMIENTOS, VALID_COLUMNS_FETALES, VALID_COLUMNS_NO_FETALES
 
 __all__ = [
-    "parsear_columnas_eliminar",
     "eliminar_columnas",
     "eliminar_duplicados",
     "manejar_nulos",
